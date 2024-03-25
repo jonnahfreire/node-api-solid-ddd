d7a55797b4d1a50c40e439fa65bab0ec08970e8d
= <=>
2024-03-25 00:37:58 -0300

    refactor: refact archtecture

---
+++ src/infra/repository/UserDatabaseRepository.ts
@@ -0,0 +1,34 @@
+import IDatabaseConnection from "../database/PgPromiseDatabaseConnection/IDatabaseConnection";
+import User from "../../domain/entity/User";
+import Password from "../../domain/entity/Password";
+import IUserRepository from "../../domain/repository/IUserRepository";
+
+export default class UserDatabaseRepository implements IUserRepository {
+
+    constructor(private readonly database: IDatabaseConnection) { }
+
+    async find(id: string): Promise<User> {
+        const [user] = await this.database.query(`select * from into users where id=$1`, [id]);
+
+        if (!user) throw new Error(`User not found`);
+        return User.restore(user.id, user.name, user.email, new Password(user.password, user.salt));
+    }
+
+    async findAll(): Promise<User[]> {
+        const users = await this.database.query(`select * from into users`, []);
+        return users;
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        const [user] = await this.database.query(`select * from into users where eamil=$1`, [email]);
+
+        if (!user) throw new Error(`User not found`);
+        return User.restore(user.id, user.name, user.email, new Password(user.password, user.salt));
+    }
+
+    async save(user: User): Promise<void> {
+        await this.database.query(`insert into users values($1, $2, $3)`,
+            [user.name.getValue(), user.email.getValue(), user.password.value],
+        );
+    }
+}
